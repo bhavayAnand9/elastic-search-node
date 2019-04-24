db9925da9c6c3623d8e45e89f0d44a6cea06a2ce
Bhavay Anand <bhavayanandcse@gmail.com>
2019-04-24 09:01:58 +0530

    successfully pipping data from mongoDB to ES

--- es.js
+++ es.js
@@ -16,7 +16,7 @@ esClient.ping({
     requestTimeout: 10000
 }, function(err){
     if(err) console.trace('ES cluster is down.');
-    else console.log('All is well');
+    else console.log('Elastic Search Cluster Is Connected');
 });
 
 MongoClient.connect(url, {useNewUrlParser: true},  function(err, db){
@@ -25,33 +25,51 @@ MongoClient.connect(url, {useNewUrlParser: true},  function(err, db){
     var collection = dbo.collection('sessions');
     collection.find().toArray(function(err, result){
         if(err) throw err;
-        console.log(util.isArray(result)? 'yes, its an array': 'no its not an array');
         // var result2 = JSON.stringify(result);
         mongoToJSON(result);
         db.close();
     });
 })
 
-function bulkIndex(index, type, data){
-    console.log(data[0].client_id, data[0].sessions);
-    let bulkBody = [];
-    data.forEach(item => {
-        bulkBody.push({
-            _index: index,
-            _type: type,
-            _id: item._id
-        })
+function generateRoutingKey(hex){
+    // console.log(hex);
+    return parseInt(hex.toString().slice(-11), 16)%7;
+}
 
-        bulkBody.push({
-            client_id: item.client_id,
-            sessions: item.sessions
-        })
+function bulkIndex(index, type, data){
+    // console.log(data[0].client_id, data[0].sessions);
+    // let bulkBody = [];
+    data.forEach(item => {
+        // bulkBody.push({
+        //     _index: index,
+        //     _type: type,
+        //     _id: item._id
+        // })
+        esClient.index({
+            index: index,
+            type: "_doc",
+            routing: generateRoutingKey(item.client_id),
+            body: {
+                client_id: {
+                    $oid: item.client_id
+                },
+                sessions: item.sessions
+            }
+        }).then(response => console.log(`item indexed, ${item.client_id}`))
+        .catch(e => {
+            console.log(`an error occured ${item.client_id}`);
+            // throw e;
+        });
+
+        // bulkBody.push({
+        //     client_id: item.client_id,
+        //     sessions: item.sessions
+        // })
     });
 
-    console.log(...bulkBody);
+    // esClient.bulk({body: bulkBody, routing: calculateRoutingValue()})
 }
 
 function mongoToJSON(data){
-    console.log(typeof data);
-    bulkIndex('sessionArr', '_doc', data);
+    bulkIndex('temp', '_doc', data);
 }
